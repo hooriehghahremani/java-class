2907734a9c1b06223cde0addc13a177d8abfdc35
Hoorie <hoorie@MacBook-Pro.local>
2021-12-23 22:08:06 +0330

    Initial Commit

---
+++ src/example1/dayOfWeek.java
@@ -0,0 +1,41 @@
+package example1;
+
+import java.util.Scanner;
+
+public class dayOfWeek {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int dayOfWeek = scanner.nextInt();
+
+        if (dayOfWeek ==1 )
+        {
+            System.out.println("Saturday");
+        }
+        else if (dayOfWeek ==2 )
+        {
+            System.out.println("Sunday");
+        }
+        else if (dayOfWeek ==3 )
+        {
+            System.out.println("Monday");
+        }
+        else if (dayOfWeek ==4 )
+        {
+            System.out.println("Thursday");
+        }
+        else if (dayOfWeek ==5 )
+        {
+            System.out.println("Wednesday");
+        }
+        else if (dayOfWeek ==6 )
+        {
+            System.out.println("Tuesday");
+        }
+        else if (dayOfWeek ==7 )
+        {
+            System.out.println("Friday");
+        }
+        else
+            System.out.println("Insert another number");
+    }
+}
